09d244df01964cbd2706bc7e53077b54f0100bab
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-29 15:28:49 -0400

    experiments with pandas pivot output to successfully reshape the data

--- dreshape.py
+++ dreshape.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
+
 df = pd.read_csv('crypto.csv', encoding='unicode_escape')
 print(df)
 
-df2 = df.pivot(index='')
+df2 = df.pivot(index='State Name', columns='Year', values="Population")
+print(df2)
