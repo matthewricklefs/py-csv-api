8741777fe8fb97bbd914920fb081e23db5d00718
Matthew <ricklefsmatthew1003@gmail.com>
2022-03-30 21:51:19 -0400

    experiments with concat method to merge pivoted table and population percentage increase value, is defeated again by pandas

--- dreshape.py
+++ dreshape.py
@@ -1,29 +1,36 @@
 import pandas as pd
 
-
 # read existing csv from fetch request
 df = pd.read_csv('ucla-long.csv')
-print(df)
+print('-'*100)
+print('OG DF: ')
+print(df.head())
+print('-'*100)
 
 # spread years horizontally / display population value for each year
-percentChange = pd.pivot_table(df, index='State Name', columns='Year', aggfunc='sum', values="Population")
-print(percentChange)
+# pivotFrame = pd.pivot_table(df, index='State Name', columns='Year', aggfunc='sum', values="Population", margins='Population', margins_name=f"2019 Factors")
+pivotFrame = pd.pivot_table(df, index='State Name', columns='Year', aggfunc='sum', values="Population")
+print('-'*100)
+print('PIVOTED DF: ')
+print(pivotFrame)
+print('-'*100)
 
 # convert population growth/decrease into percentage value
-print(percentChange.pct_change())
-
-
-
-
+percentChange = pd.pivot_table(df, index="State Name", columns='Year', aggfunc='sum', values='Population').pct_change()
+print('-'*100)
+print('PERCENT CHANGE DF: ')
+print(percentChange)
+print('-'*100)
 
 # spread years horizontally / display population value for each year
-# df2 = df.pivot(index='State Name', columns='Year', values="Population")
-# print(df2)
+# df2 = percentChange.pivot(df, index='State Name', columns='Year', values=["Population"])
+print('-'*100)
+print('VALUE COUNT: ')
+pop_percent = pd.concat([pivotFrame, percentChange], axis='columns', sort=False)
+print(pop_percent)
+print('-'*100)
+
+# -------
 
 # export to csv
 # df2.to_csv('ucla-data.csv')
-
-# convert 
-# new_df = df.groupby('Year')['Population'].value_counts(normalize=True)
-# new_df = new_df.mul(100).rename('Percent')
-# print(new_df)
